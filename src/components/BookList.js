762255533d277adcead818f2f2a0467ffa25ad84
Damien de Lartigue <damien@delartigue.co.uk>
2021-07-07 12:28:55 +0100

    refactor components to use useContext hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-    static contextType = ThemeContext;
+const BookList = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
 
-    render() { 
-        const { isLightTheme, light, dark} = this.context;
-        const theme = isLightTheme ? light : dark;
-        return ( 
-            <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-                <ul>
-                    <li style={{ background: theme.ui }}>the pillars of the earth</li>
-                    <li style={{ background: theme.ui }}>the final empire</li>
-                    <li style={{ background: theme.ui }}>dune</li>
-                </ul>
-            </div>
-         );
-    }
+    return ( 
+        <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+            <ul>
+                <li style={{ background: theme.ui }}>the pillars of the earth</li>
+                <li style={{ background: theme.ui }}>the final empire</li>
+                <li style={{ background: theme.ui }}>dune</li>
+            </ul>
+        </div>
+     );
 }
  
 export default BookList;
